7c186fe3b30ddb355b87e4bda27bce53ba8a8f5c
oleh <oleht@wizardsdev.com>
2021-12-04 16:03:17 +0200

    init

--- router/router.js
+++ router/router.js
@@ -1,4 +1,4 @@
-const queryString = require('query-string');
+const URL = require('url');
 
 const { GET_USER, CREATE_USER } = require('../constants/routes');
 const { getSingleUser, createUser } = require('../controllers/user.controller');
@@ -6,15 +6,14 @@ const { getSingleUser, createUser } = require('../controllers/user.controller');
 const router = async ({ req, res, body }) => {
   let result, error;
   const { method, url } = req;
-  const [path, query] = url.split('?');
-  const parsedQuery = queryString.parse(query);
+  const { query, pathname } = URL.parse(url, true);
 
   switch (true) {
-    case method === 'GET' && path === GET_USER:
-      ({ result, error } = await getSingleUser(parsedQuery));
+    case method === 'GET' && pathname === GET_USER:
+      ({ result, error } = await getSingleUser(query));
       break;
 
-    case method === 'POST' && path === CREATE_USER:
+    case method === 'POST' && pathname === CREATE_USER:
       ({ result, error } = await createUser(body));
       break;
 
